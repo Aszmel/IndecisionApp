bea1d96105ee5d472236a16538a95a238b943bf1
Aszmel <aszmel@o2.pl>
2018-11-01 20:38:24 +0100

    added scss loader and styles.scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  mode: "none",
   entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
@@ -15,7 +16,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
